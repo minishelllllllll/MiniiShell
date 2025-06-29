7e9449accbd014d861c2b497e38a6c39dc34d449
MoustaidHind <151015622+MoustaidHind@users.noreply.github.com>
2025-06-29 15:15:53 +0100

    resize (new_env)

--- src/execution/key_value.c
+++ src/execution/key_value.c
@@ -1,36 +1,41 @@
 #include "../../includes/minishell.h"
 
+t_env *split_key_value(int is_path_exported, char *env, char **envp)
+{
+	t_env		*newnode;
+	char 		*value;
+	char 		**key;
+
+	newnode = (t_env *)malloc(sizeof(t_env));
+	if(!newnode)
+		return(NULL);
+	value = ft_strchr(env, '='); // search for the fisrt (=) and return pointer to it
+	key = ft_split(env, '='); // split the command with (=);
+	if(!key)
+		return(NULL);
+	newnode->key = ft_strdup(key[0]); //duplicate the first argument
+	newnode->value = ft_strdup(value + 1); //dup the string, +1 to skiip (=), like (=/hind/....)
+	newnode->next = 0;
+	if(ft_strcmp(newnode->key, "PATH") == 0 && (!envp || !envp[0]) && !is_path_exported) // flaged with 2 to not printed in (env, export)
+	{
+		newnode->flag_exported = 2;
+		is_path_exported = 1;
+	}
+	else
+		newnode->flag_exported = 1; //if has a value (true)
+	return(newnode);
+}
+
 t_env *new_env(char *env, char **envp)
 {
 	static int 	is_path_exported;
 	t_env		*newnode;
-	char 		*value;
-	char 		**key;
 	
-	// is_path_exported = 0;
 	newnode = (t_env *)malloc(sizeof(t_env));
 	if(!newnode)
 		return(NULL);
-	
 	if(ft_strchr(env, '=') != NULL)
-	{
-		value = ft_strchr(env, '='); // search for the fisrt (=) and return pointer to it
-		key = ft_split(env, '='); // split the command with (=);
-		if(!key)
-			return(NULL);
-	
-		newnode->key = ft_strdup(key[0]); //duplicate the first argument
-		newnode->value = ft_strdup(value + 1); //dup the string, +1 to skiip (=), like (=/hind/....)
-		newnode->next = 0;
-
-		if(ft_strcmp(newnode->key, "PATH") == 0 && (!envp || !envp[0]) && !is_path_exported) // flaged with 2 to not printed in (env, export)
-		{
-			newnode->flag_exported = 2;
-			is_path_exported = 1;
-		}
-		else
-			newnode->flag_exported = 1; //if has a value (true)	
-	}
+		newnode = split_key_value(is_path_exported, env, envp);
 	else
 	{
 		newnode->key = ft_strdup(env); //duplicate env 
@@ -38,7 +43,6 @@ t_env *new_env(char *env, char **envp)
 		newnode->flag_exported = 0; //if just the key (false)
 		newnode->next = 0;
 	}
-
 	return(newnode);
 }
 
